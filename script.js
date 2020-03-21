0665e2828b40d03dae843ab502a9e78b5f78b334
ignaskas1 <ignaskas1@gmail.com>
2020-03-21 20:40:37 +0200

    just fuck me side ways and fuck jquery

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ let genres = {
     scfi: 0
 };
 
+// path we take states
 let currentState;
 let state2 = {
     name: 'forest',
     locationX: 10,
     locationY: 20,
     possibleOutcomes: [
-        {}
+
     ],
     complicatedBehaviour: function(){
         // doNothing
@@ -25,18 +26,18 @@ let state3 = {
     locationX: 20,
     locationY: 30,
     possibleOutcomes: [
-        {}
+
     ],
     complicatedBehaviour: function(){
         // doABarrelRoll()
     }
 };
 let state4 = {
-    name: 'path',
+    name: 'road',
     locationX: 40,
     locationY: 30,
     possibleOutcomes: [
-        {}
+
     ],
     complicatedBehaviour: function(){
         // doNothing
@@ -57,112 +58,7 @@ let state1 = {
     }
 };
 
-currentState = state1;
 let buttons = [];
-updatePossibilities();
-
-function updatePossibilities() {
-    //hide old buttons
-    for(let button of buttons){
-        // button.applyCss ('DISPLAY: NONE')
-    }
-    buttons = [];
-
-    for (let aState of currentState.possibleOutcomes) {
-
-        // let button = getElementById(aState.name)
-        // button.applyCss(UNHIDE_ME -> add DISPLAY BLOCK instead of NONE)
-        // button.applyOnClick (click)
-        button.click(function() {
-            aState.complicatedBehaviour()
-            click(aState)
-        });
-
-        buttons.push(/*new button that has click(aState) as behaviour */);
-    }
-}
-
-function click(state) {
-    moves+=1;
-    makeMove(state.locationX, state.locationY); /*-> turning, movement*/
-    currentState = state;
-    updatePossibilities(); /*-> hide old buttons, create new buttons for all the possible next outcomes*/
-}
-
-
-// cordinate system for animation
-// let roadlist = [
-//     [[{name: "forest1", locationX: 284, locationY: 551}, {
-//         name: "forest2",
-//         locationX: 392,
-//         locationY: 377
-//     }, {name: "forest3", locationX: 866, locationY: 329}], [{
-//         name: "city",
-//         locationX: 1002,
-//         locationY: 406
-//     }, {name: "city2", locationX: 1064, locationY: 426}, {
-//         name: "city3",
-//         locationX: 1074,
-//         locationY: 512
-//     }], [{name: "magetower1", locationX: 1134, locationY: 650}, {
-//         name: "magetower2",
-//         locationX: 1454,
-//         locationY: 550
-//     }, {name: "magetower3", locationX: 1634, locationY: 578}]],
-//     [[{name: "road1", locationX: 430, locationY: 668}, {name: "road2", locationX: 856, locationY: 645}, {
-//         name: "road3",
-//         locationX: 896,
-//         locationY: 636
-//     }], [{name: "jungle1", locationX: 921, locationY: 683}, {
-//         name: "jungle2",
-//         locationX: 985,
-//         locationY: 695
-//     }, {name: "jungle3", locationX: 1013, locationY: 775}], [{
-//         name: "backtocamp1",
-//         locationX: 991,
-//         locationY: 685
-//     }, {name: "backtocamp2", locationX: 925, locationY: 681}, {
-//         name: "backtocamp3",
-//         locationX: 899,
-//         locationY: 649
-//     }], [{name: "tocityafterjungle1", locationX: 989, locationY: 675}, {
-//         name: "tocityafterjungle2",
-//         locationX: 1131,
-//         locationY: 651
-//     }, {name: "tocityafterjungle3", locationX: 1091, locationY: 545}], [{
-//         name: "magetower1",
-//         locationX: 1134,
-//         locationY: 650
-//     }, {name: "magetower2", locationX: 1454, locationY: 550}, {name: "magetower3", locationX: 1634, locationY: 578}]],
-//     [[{name: "portal1", locationX: 598, locationY: 898}, {
-//         name: "portal2",
-//         locationX: 1141,
-//         locationY: 365
-//     }, {name: "portal3", locationX: 1498, locationY: 360}], [{
-//         name: "tomagetower1",
-//         locationX: 1579,
-//         locationY: 381
-//     }, {name: "tomagetower2", locationX: 1679, locationY: 451}, {
-//         name: "tomagetower3",
-//         locationX: 1667,
-//         locationY: 511
-//     }]],
-// ];
-
-//TODO: change the cordinates to fit the map
-//Path we take
-// let loc = [
-//     {
-//         name: "Current_Position", // 0 for cheking withc direction we are facing
-//         locationX: 332,
-//         locationY: 643
-//     },
-//     {
-//         name: "New_Position", // 1  for cheking withc direction we are facing
-//         locationX: 0,
-//         locationY: 0
-//     }
-// ];
 
 // atributes
 let int = 4;
@@ -224,11 +120,11 @@ function witchsidedoiface(locationX, locationY) {
     // console.log("f " + locationX, "f+ " + locationY);
     if ((currentState.locationY > locationY) && (currentState.locationX > locationX)) {  //we are moving NW -- top left
         $('#player').css('background-position', '120px 0px');
-    } else if ((loc[0].locationY < locationY) && (loc[0].locationX < locationX)) { //we are moving SE -- bottom rigth
+    } else if ((currentState.locationY < locationY) && (currentState.locationX < locationX)) { //we are moving SE -- bottom rigth
         $('#player').css('background-position', '60px 0px');
-    } else if ((loc[0].locationY > locationY) && (loc[0].locationX < locationX)) { //we are moving NE -- top rigth
+    } else if ((currentState.locationY > locationY) && (currentState.locationX < locationX)) { //we are moving NE -- top rigth
         $('#player').css('background-position', '0px 0px');
-    } else if ((loc[0].locationY < locationY) && (loc[0].locationX > locationX)) { //we are moving SW -- bottom left
+    } else if ((currentState.locationY < locationY) && (currentState.locationX > locationX)) { //we are moving SW -- bottom left
         $('#player').css('background-position', '200px 0px');
     }
 }
@@ -283,44 +179,42 @@ function makeMove(locationX, locationY) {
     witchsidedoiface(locationX, locationY);
     $('#player').animate({left: locationX - 30, top: locationY - 80}, 5000);
     // setTimeout(flavortextshowhide, 5000);
-    // loc[0].locationX = loc[1].locationX;
-    // loc[0].locationY = loc[1].locationY;
+
 }
 
-// MOVEMENT---------------
-// TODO: set timeout as a variable
-// moves from first card
-function firstcard() {
-    moves += 1;
-    for (i = 0; i < roadlist.length; i++) {
-        console.log(roadlist[0][moves][i].locationX + " " + roadlist[0][moves][i].locationY);
-        makeMove(roadlist[0][moves][i].locationX, roadlist[0][moves][i].locationY);
+currentState = state1;
+updatePossibilities();
+
+function updatePossibilities() {
+    // buttons = [];
+    console.log(currentState.possibleOutcomes);
+    for (let aState of currentState.possibleOutcomes) {
+
+        // let button = $('#' + aState.name);
+        $('#' + aState.name).css('display', 'block');
+        // $('button').click (click);
+        console.log(aState.name);
+        $('#' + aState.name).click(function() {
+            console.log("just fuck me");
+            aState.complicatedBehaviour();
+            click(aState)
+        });
+
+        // buttons.push(button);
     }
-    flavortextshowhide();
-    setTimeout(flavortextshowhide, 5000);
 }
 
-// moves from second card
-function secondcard() {
-    moves += 1;
-    for (i = 0; i < roadlist.length; i++) {
-        console.log(roadlist[1][moves][i].locationX + " " + roadlist[0][moves][i].locationY);
-        makeMove(roadlist[1][moves][i].locationX, roadlist[1][moves][i].locationY);
+function click(state) {
+    console.log(state);
+    makeMove(state.locationX, state.locationY); /*-> turning, movement*/
+    // hide old buttons
+    for(let aState of currentState.possibleOutcomes){
+        $('#' + aState.name).css('display', 'none');
     }
-    flavortextshowhide();
-    setTimeout(flavortextshowhide, 5000);
+    currentState = state;
+    updatePossibilities(); /*-> hide old buttons, create new buttons for all the possible next outcomes*/
 }
 
-// moves from third card
-function thirdcard() {
-    moves += 1;
-    for (i = 0; i < roadlist.length; i++) {
-        console.log(roadlist[2][moves][i].locationX + " " + roadlist[0][moves][i].locationY);
-        makeMove(roadlist[2][moves][i].locationX, roadlist[2][moves][i].locationY);
-    }
-    flavortextshowhide();
-    setTimeout(flavortextshowhide, 5000);
-}
 
 //MOVMENT ENDS---------------
 
