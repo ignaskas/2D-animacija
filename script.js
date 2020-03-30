e182c5dbbe18a2802f0f4cbe55b059c329a05af2
ignaskas1 <ignaskas1@gmail.com>
2020-03-30 23:04:52 +0300

    MOVEMENT IS FUCKING DONE YEZZUZ FUCK ME

--- script.js
+++ script.js
@@ -10,13 +10,675 @@ let genres = {
 // game states / movement cordinates / atribute checks
 let currentState;
 
+
+let state30 = {
+    name: 'dragon_figth_win_bcs_armor',
+    locationX: 1659,
+    locationY: 503,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1639,
+            locationY: 565
+        },
+        {
+            locationX: 1667,
+            locationY: 523
+        },
+        {
+            locationX: 1659,
+            locationY: 503
+        }
+    ],
+    attributeCheck: function() {
+        if (armor === true){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        youwin();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+//TODO: fix the cordinates
+//lose to boss
+let state29 = {
+    name: 'dragon_figth_lose',
+    locationX: 1605,
+    locationY: 579,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1137,
+            locationY: 645
+        },
+        {
+            locationX: 1483,
+            locationY: 545
+        },
+        {
+            locationX: 1605,
+            locationY: 579
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.horror += 1;
+        youhavedied();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//ignore shop go to dragon
+let state28 = {
+    name: 'ignore_shop',
+    locationX: 1605,
+    locationY: 579,
+    possibleOutcomes: [
+        state29,    //YOU DIE
+        state30,    //WIN BCS ARMOR
+
+    ],
+    steps: [
+        {
+            locationX: 1137,
+            locationY: 645
+        },
+        {
+            locationX: 1483,
+            locationY: 545
+        },
+        {
+            locationX: 1605,
+            locationY: 579
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//steal armor with your agy check
+let state27 = {
+    name: 'steal_armor_after_merchant',
+    locationX: 1605,
+    locationY: 579,
+    possibleOutcomes: [
+        state29,    //YOU DIE
+        state30,    //WIN BCS ARMOR
+    ],
+    steps: [
+        {
+            locationX: 1137,
+            locationY: 645
+        },
+        {
+            locationX: 1483,
+            locationY: 545
+        },
+        {
+            locationX: 1605,
+            locationY: 579
+        }
+    ],
+    attributeCheck: function() {
+        if (agy >= 6){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        getarmor();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// buy armor with your bag of coins
+let state26 = {
+    name: 'shop_city_buy_armor',
+    locationX: 1605,
+    locationY: 579,
+    possibleOutcomes: [
+        state29,    //YOU DIE
+        state30,    //WIN BCS ARMOR
+    ],
+    steps: [
+        {
+            locationX: 1137,
+            locationY: 645
+        },
+        {
+            locationX: 1483,
+            locationY: 545
+        },
+        {
+            locationX: 1605,
+            locationY: 579
+        }
+    ],
+    attributeCheck: function() {
+        if (bagOfCoins === true){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        getarmor();
+        removecoins();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// YOU LOSE
+let state25 = {
+    name: 'city_after_lizard_men',
+    locationX: 1091,
+    locationY: 558,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1101,
+            locationY: 560
+        },
+        {
+            locationX: 1097,
+            locationY: 558
+        },
+        {
+            locationX: 1091,
+            locationY: 558
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        jailed();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+//TODO: fix the cordinates
+// go to city after returing to merchant
+let state24 = {
+    name: 'go_to_city_after_returing_to_merchant',
+    locationX: 1113,
+    locationY: 580,
+    possibleOutcomes: [
+        state26,    //BUY ARMOR
+        state27,    //STEAL ARMOR
+        state28     //IGNORE ARMOR
+    ],
+    steps: [
+        {
+            locationX: 955,
+            locationY: 678
+        },
+        {
+            locationX: 1133,
+            locationY: 580
+        },
+        {
+            locationX: 1113,
+            locationY: 580
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// go to city after lizard men are dead
+let state23 = {
+    name: 'lizard_men_go_to_city',
+    locationX: 1099,
+    locationY: 573,
+    possibleOutcomes: [
+        state25,    //GO TO CITY
+    ],
+    steps: [
+        {
+            locationX: 987,
+            locationY: 683
+        },
+        {
+            locationX: 1125,
+            locationY: 649
+        },
+        {
+            locationX: 1099,
+            locationY: 573
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        bagofcoins();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// go back to merchant after kiling lizardmen
+let state22 = {
+    name: 'lizard_men_go_back_to_merchant',
+    locationX: 899,
+    locationY: 647,
+    possibleOutcomes: [
+        state24,    //GO TO CITY
+    ],
+    steps: [
+        {
+            locationX: 993,
+            locationY: 677
+        },
+        {
+            locationX: 915,
+            locationY: 677
+        },
+        {
+            locationX: 899,
+            locationY: 647
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        bagofcoins();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// ignore merchant and go to city
+let state21 = {
+    name: 'ignore_merchant',
+    locationX: 1091,
+    locationY: 555,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 947,
+            locationY: 675
+        },
+        {
+            locationX: 1123,
+            locationY: 647
+        },
+        {
+            locationX: 1071,
+            locationY: 555
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// help merchant
+let state20 = {
+    name: 'help_merchant',
+    locationX: 1025,
+    locationY: 777,
+    possibleOutcomes: [
+        state22,    //GO BACK TO ME
+        state23     //GO TO CITY
+    ],
+    steps: [
+        {
+            locationX: 993,
+            locationY: 677
+        },
+        {
+            locationX: 1003,
+            locationY: 731
+        },
+        {
+            locationX: 1025,
+            locationY: 777
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// win if you have blaster
+let state19 = {
+    name: 'after_portal_win',
+    locationX: 1677,
+    locationY: 494,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1579,
+            locationY: 386
+        },
+        {
+            locationX: 1681,
+            locationY: 446
+        },
+        {
+            locationX: 1677,
+            locationY: 494
+        }
+    ],
+    attributeCheck: function() {
+        if (blaster === true){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.scifi += 1;
+        youwin();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// if you dont have blaster you die to dragon
+let state18 = {
+    name: 'after_portal_dragon_figth_lose',
+    locationX: 1677,
+    locationY: 494,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1579,
+            locationY: 386
+        },
+        {
+            locationX: 1681,
+            locationY: 446
+        },
+        {
+            locationX: 1677,
+            locationY: 494
+        }
+    ],
+    attributeCheck: function() {
+        return true
+    },
+    complicatedBehaviour: function () {
+        genres.scifi += 1;
+        youhavedied();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//WIN game lower discount
+let state17 = {
+    name: 'dragon_steal',
+    locationX: 1663,
+    locationY: 451,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1669,
+            locationY: 525
+        },
+        {
+            locationX: 1683,
+            locationY: 471
+        },
+        {
+            locationX: 1663,
+            locationY: 451
+        }
+    ],
+    attributeCheck: function() {
+        return true
+    },
+    //TODO: change this outcome to a difrtent function
+    complicatedBehaviour: function () {
+        genres.strategy += 1;
+        youwin();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//Int agy flask check to poison dragon
+let state16 = {
+    name: 'dragon_posion',
+    locationX: 1663,
+    locationY: 451,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1669,
+            locationY: 525
+        },
+        {
+            locationX: 1683,
+            locationY: 471
+        },
+        {
+            locationX: 1663,
+            locationY: 451
+        }
+    ],
+    attributeCheck: function() {
+        if (int >= 6 && agy >= 7 && flaskfull === true){
+            return true;
+        } else {
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.rpg += 1;
+        youwin();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// LOSE DRAGON FIGTH
+let state15 = {
+    name: 'dragon_run',
+    locationX: 1663,
+    locationY: 451,
+    possibleOutcomes: [
+
+    ],
+    steps: [
+        {
+            locationX: 1669,
+            locationY: 525
+        },
+        {
+            locationX: 1683,
+            locationY: 471
+        },
+        {
+            locationX: 1663,
+            locationY: 451
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.horror += 1;
+        youhavedied();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//ASK FOR QUEST
+let state14 = {
+    name: 'ask_for_quest',
+    locationX: 1597,
+    locationY: 571,
+    possibleOutcomes: [
+        state15,    //LOSE DRAGON FIGTH
+        state16,    //POISION DRAGON
+        state17     //STEAL FROM DRAGON
+    ],
+    steps: [
+        {
+            locationX: 1133,
+            locationY: 643
+        },
+        {
+            locationX: 1467,
+            locationY: 539
+        },
+        {
+            locationX: 1597,
+            locationY: 571
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.adventure += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+// STEAL ARMOR AGY CHECK
+let state13 = {
+    name: 'steal_armor',
+    locationX: 1597,
+    locationY: 571,
+    possibleOutcomes: [
+        state15,    //LOSE DRAGON FIGTH
+        state16,    //POISION DRAGON
+        state17     //STEAL FROM DRAGON
+    ],
+    steps: [
+        {
+            locationX: 1133,
+            locationY: 643
+        },
+        {
+            locationX: 1467,
+            locationY: 539
+        },
+        {
+            locationX: 1597,
+            locationY: 571
+        }
+    ],
+    attributeCheck: function() {
+        if (agy >= 7){
+            return true;
+        } else{
+            return false;
+        }
+    },
+    complicatedBehaviour: function () {
+        genres.rpg += 1;
+        getarmor();
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
+//TO DRAGON AFTER CITY
+let state12 = {
+    name: 'no_money_for_armor',
+    locationX: 1597,
+    locationY: 571,
+    possibleOutcomes: [
+        state15,    //LOSE DRAGON FIGHT
+        state16,    //POISION DRAGON
+        state17     //STEAL FROM DRAGON
+    ],
+    steps: [
+        {
+            locationX: 1133,
+            locationY: 643
+        },
+        {
+            locationX: 1467,
+            locationY: 539
+        },
+        {
+            locationX: 1597,
+            locationY: 571
+        }
+    ],
+    attributeCheck: function() {
+        return true;
+    },
+    complicatedBehaviour: function () {
+        genres.strategy += 1;
+        flavortextshowhide();
+        setTimeout(flavortextshowhide, 6000);
+    }
+};
+
 //GO TO CITY AFTER FOREST
 let state11 = {
     name: 'go_tocity',
     locationX: 1102,
     locationY: 556,
     possibleOutcomes: [
-
+        state12,    //No Money for armor
+        state13,    //agy check steal armor
+        state14     //Ask for directions
     ],
     steps: [
         {
@@ -36,7 +698,7 @@ let state11 = {
         return true;
     },
     complicatedBehaviour: function () {
-        genres.rpg += 1;
+        genres.adventure += 1;
         flavortextshowhide();
         setTimeout(flavortextshowhide, 6000);
     }
@@ -149,7 +811,8 @@ let state7 = {
     locationX: 894,
     locationY: 671,
     possibleOutcomes: [
-
+        state20,    //HELP
+        state21,    //IGNORE
     ],
     steps: [
         {
@@ -181,7 +844,8 @@ let state6 = {
     locationX: 1510,
     locationY: 344,
     possibleOutcomes: [
-
+        state18,    //LOSE
+        state19,    //WIN ONLY IF YOU HAVE BLASTER
     ],
     steps: [
         {
@@ -390,6 +1054,9 @@ let pointstospend = 5;
 // move counter
 let moves = -1;
 
+//genre name after wining
+let genrewin = "";
+
 // items
 let glassKey = true;
 let armor = false;
@@ -465,11 +1132,8 @@ function chekbest(genres) {
         max = (num > max && num) || max;
     }
 
-    return maxname;
+    genrewin = maxname;
 }
-
-//TODO: console.log needs to be removed and replaced with proper call function
-console.log(chekbest(genres));
 
 //TODO: remove this when pathing is done
 //prints location of mouse curson onclick
@@ -549,6 +1213,12 @@ window.onload = function () {
     $('#bagofcoins').hide();
     $('#didntSpendAllPointsBeforePressingStart').hide();
     $('#blackout').fadeOut("slow");
+
+    $('#rpg').hide();
+    $('#horror').hide();
+    $('#adve').hide();
+    $('#stra').hide();
+    $('#scifi').hide();
 };
 // start the game only if all atribute points were spent
 function startgame() {
@@ -591,11 +1261,49 @@ function filltheflask() {
     flask = false;
     flaskfull = true;
 }
+// player has acquired armor show armor icon set armor value to true
+function getarmor() {
+    $('#armor').show();
+    armor = true;
+}
+// show bag of coins icon set bagofcoins variable to true
+function bagofcoins() {
+    $('#bagofcoins').show();
+    bagOfCoins = true;
+}
+//hide bag of coins icon set bagofcoins variable to false
+function removecoins() {
+    $('#bagofcoins').hide();
+    bagOfCoins = false;
+}
 //items end ---------------------------------------
 //ENDING---------------------------
+function youwin() {
+    chekbest(genres);
+    whatdiscountwegive();
+}
+
+function whatdiscountwegive() {
+    if (genrewin === "rpg"){
+        $('#rpg').show();
+    } else if (genrewin === "horrror"){
+        $('#horror').show();
+    } else if (genrewin === "adventure"){
+        $('#adve').show();
+    } else if (genrewin === "strategy"){
+        $('#stra').show();
+    } else if (genrewin === "scifi"){
+        $('#scifi').show();
+    }
+}
 //DEAD---
 function youhavedied() {
     $('#flavor_text').css("background-image", "url(./assets/youdie.jpg)");
+}
+
+function jailed() {
+    $('#flavor_text').css("background-image", "url(./assets/youdie.jpg)");
+
 }
 //DEAD END---
 //ENDING END-------------------------------
